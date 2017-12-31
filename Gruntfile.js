1844b72b4f1b1c8e72bf770bcf8dc2368479eef3
xenogonx <pablez@wp.pl>
2017-12-31 16:09:31 +0100

    small changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,58 @@
 module.exports = function(grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        sass: {
-            options: {
-                sourceMap: true,
-                outputStyle: 'expanded'
-            },
-            dist: {
-                files: {
-                    'css/style.css': 'sass/style.sass',
-                }
-            }
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true,
+        outputStyle: 'expanded'
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.sass',
+        }
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['**/*.{png,jpg,gif}'], 
+          dest: 'img/small_size/'
+        }]
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
         },
-        imagemin: { 
-            dynamic: { 
-                files: [{ 
-                    expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif}'], 
-                dest: 'img/small_size/' 
-                }] 
-            } 
-        },
-        watch: {
-            scripts: {
-                files: ['sass/*.sass'],
-                tasks: ['sass'],
-                options: {
-                    spawn: false
-                },
-            }
-        },
-        
-        browserSync: {
-            bsFiles: {
-                src : ['**/*.{css,html}'],
+      }
+    },
 
-            },
-            
-            options: {
-                    watchTask: true,
-                    server: './',
-                    browser: "firefox"
-            }
-        },
-        
-    });
+    browserSync: {
+      bsFiles: {
+        src : ['**/*.{css,html}'],
 
-    // Load the plugins tasks
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-browser-sync');
-   
+      },
 
-    // Default task(s).
-    grunt.registerTask('default', ['browserSync', 'sass', 'imagemin', 'watch']);
+      options: {
+          watchTask: true,
+          server: './',
+          browser: "firefox"
+      }
+    },
+
+  });
+
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserSync', 'sass', 'imagemin', 'watch']);
 };
